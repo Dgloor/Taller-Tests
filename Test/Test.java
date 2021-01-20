7d645403c91b3ff04564ec58944e43e7a23b19c2
johangilces <jjgilces@espol.edu.ec>
2021-01-20 09:41:10 -0500

    empleados

--- Test/Test.java
+++ Test/Test.java
@@ -9,4 +9,15 @@ class Test {
 		fail("Not yet implemented");
 	}
 
+	 public Test() {
+        e1 = new Employee(100f, "USD", 1f, EmployeeType.Manager);
+        e2 = new Employee(100f, "USD", 1f, EmployeeType.Supervisor);
+        e3 = new Employee(100f, "USD", 1f, EmployeeType.Worker);
+        e4 = new Employee(100f, "EUR", 1f, EmployeeType.Manager);
+        e5 = new Employee(100f, "EUR", 1f, EmployeeType.Supervisor);
+        e6 = new Employee(100f, "EUR", 1f, EmployeeType.Worker);
+
+        numMes = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonthValue();
+    }
+
 }
